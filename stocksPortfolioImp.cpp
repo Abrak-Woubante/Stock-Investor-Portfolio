7c56e7197e57694b64067be0f1508b4606414df7
Abrak-Woubante <46204428+Abrak-Woubante@users.noreply.github.com>
2024-01-01 19:15:37 -0800

    Update stocksPortfolioImp.cpp

--- stocksPortfolioImp.cpp
+++ stocksPortfolioImp.cpp
@@ -1,28 +1,3 @@
-/*
-Name:  NSHE: , CLASS-SECTION: CS202, ASSIGNMENT: 3
-Description: In investing, what is comfortable is rarely profitable.
-Input: Develop stockInvestor(base class) that can be useful to set 
-firstName, lastName and ID; set the investors data using setters or 
-constructors from stockInvestors class; 
-investorType is a derived (inheritance: is-a relationship) class from 
-stockInvestor class and is useful to set the investor type, stock name, 
-number of stocks, and stock price; 
-stocksPortfolio is a base class with an object of other 
-class(investorType) (Composition: has-a relationshipinvestorType).
-stocksPortfolio is to set 3 different prices;
-Output: Write the investors data to a file(investorsData.txt) following 
-the driver code in stocksMain.cpp. The objects declaration of different 
-classes, using constructors, setters, member functions to calculate the
-portfolio of each investor. The investors' portfolios (reports) are 
-printed on terminal.
-In fileIO:
-This class has only one member function. The member function returns the 
-number of entries
-Read the data from (investorsData.txt) file; use constructors, setters, 
-member functions to calculate the portfolio of each investor. Finally,
-write the investors' portfolios (reports) to an outputfile 
-(investorsPortfolio.txt). 
-*/
 #include "stocksPortfolio.h"
 
 stocksPortfolio::stocksPortfolio(){
@@ -38,7 +13,7 @@ stocksPortfolio::stocksPortfolio(double pOne, double pTwo, double pThree, invest
     priceOne=pOne;
     priceTwo=pTwo;
     priceThree=pThree;
-    investorType invTypeObj=invTemp; // is this right ?
+    investorType invTypeObj=invTemp; 
 
 }
 
@@ -46,7 +21,7 @@ void stocksPortfolio::setPortfolio(double pOne, double pTwo, double pThree, inve
     priceOne=pOne;
     priceTwo=pTwo;
     priceThree=pThree;
-    investorType invTypeObj=invTemp;//ask if this is right 
+    investorType invTypeObj=invTemp;
 }
 
 void stocksPortfolio::printPriceChanges(ostream &output) const{
@@ -56,7 +31,7 @@ void stocksPortfolio::calculateStockValue(investorType invTemp, ostream &output)
     double initialSV =invTemp.getStockPrice()*invTemp.getStockCount();
     double price1= priceOne*invTemp.getStockCount();
     double price2= priceTwo*invTemp.getStockCount();
-    double price3= priceThree*invTemp.getStockCount();//ONLY USE THIS
+    double price3= priceThree*invTemp.getStockCount();
     double diff3= price3 - initialSV;
     if(!invTypeObj.checkInvestorID(invTemp.getID())||invTemp.getStockCount()<0||invTemp.getStockPrice()<0){
      initialSV =0;//set everything to 0 if error 
@@ -80,14 +55,12 @@ void stocksPortfolio::calculateStockValue(investorType invTemp, ostream &output)
     printPriceChanges(output);  
     output<<endl;
     string stars; 
-    stars.append(65, '*'); //print some stars
+    stars.append(65, '*'); 
     output << stars << endl;
 }
 
 void stocksPortfolio::writePriceChangesToFile(ostream &output) const{
     output<<priceOne<<setw(10)<<priceTwo<<setw(10)<<priceThree<<endl;
-    //set wideths for last three
-
 }
 
 double stocksPortfolio::getInvestorPrice() const{
