0cd201f1a797c0e0f3e7c8dff853b24d0f297d3a
Abrak-Woubante <46204428+Abrak-Woubante@users.noreply.github.com>
2024-01-01 18:56:53 -0800

    Update stocksPortfolio.h

--- stocksPortfolio.h
+++ stocksPortfolio.h
@@ -6,47 +6,31 @@
 
 class stocksPortfolio{
 private:
-    double priceOne;  //variable to store the priceOne
-    double priceTwo;  //variable to store the priceTwo
-    double priceThree;  //variable to store the priceThree
-    double stockValue;  //variable to store the stockValue
-    investorType invTypeObj; // object of investorType (Composition)
+    double priceOne; 
+    double priceTwo;  
+    double priceThree;  
+    double stockValue; 
+    investorType invTypeObj;
 
 public:
-	stocksPortfolio(); //Default constructor
+	stocksPortfolio(); 
 	stocksPortfolio(double pOne, double pTwo, double pThree, investorType invTemp);
-	//parameterized constructor
-  	//Initialize the private member varaibles: priceOne, priceTwo, and priceThree 
-  	//according to the parameters
-  	//Initialize private member object of other class according to the given parameter
 	
 	void setPortfolio(double pOne, double pTwo, double pThree, investorType invTemp);
-	//Initialize the private member varaibles: priceOne, priceTwo, and priceThree 
-  	//according to the parameters
-  	//Initialize private member object of other class according to the given parameter
-
-    //To print three price changes
-    void printPriceChanges(ostream &output) const;
+	
+        void printPriceChanges(ostream &output) const;
     
 	void calculateStockValue(investorType invTemp, ostream &output);
-	//Calculate stockValue- multiply stockPrice with stockCount 
-							//(both from investor type; use getters)
-	//multiply stockCount with each new price and print statements accordingly
-	//Finally print stockValue - Show Loss/Profit accordingly
-	//Check sample output for more details 
-
-
-	void writePriceChangesToFile(ostream &output) const; 
-	//Write priceOne, priceTwo, and priceThree to the investorsData.txt
-	//Check sample output, the prices should be in same line as id, name, etc.
 	
-	double getInvestorPrice() const; //get investor price from investorType
+	void writePriceChangesToFile(ostream &output) const; 
+	
+	
+	double getInvestorPrice() const; 
 	double getPriceOne() const;
 	double getPriceTwo() const;
 	double getPriceThree() const;
 	double getStockValue() const;
-    //getter(Accessor) functions to return private member variables
-    //accordingly
+    
 	
 };
 
